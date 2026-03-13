96ecfb47836137189b7f4a9071348770fa8282cb
Kirulu chamika <kiruluchamika2002@gmail.com>
2026-03-13 20:52:43 +0530

    Fix

--- tests/codegen.spec.js
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.wikipedia.org/');
-  await page.getByRole('link', { name: 'English 7,141,000+ articles' }).click();
-  await page.getByRole('searchbox', { name: 'Search Wikipedia' }).click();
-  await page.getByRole('combobox', { name: 'Search Wikipedia' }).fill('pplaywright');
-  await page.getByRole('button', { name: 'Search' }).click();
-});
